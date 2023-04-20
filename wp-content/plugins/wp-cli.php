64fe2a0611cdbffc20dd154bd4023ebeff41f3b2
JamesVanWaza <jgacuca@gmail.com>
2023-04-20 15:53:42 -0400

    Updated the reindex_post function

--- wp-content/plugins/wp-cli.php
+++ wp-content/plugins/wp-cli.php
@@ -7,57 +7,58 @@ if (!(defined('WP_CLI') && WP_CLI)) {
 class Algolia_Command {
     public function reindex_post($args, $assoc_args) {
         global $algolia;
+        global $table_prefix;
+    
+        $type = isset($assoc_args['type']) ? $assoc_args['type'] : 'post';
+    
+        $indexName = $table_prefix.$type;
+        
         $index = $algolia->initIndex(
-            apply_filters('algolia_index_name', 'post')
+            apply_filters('algolia_index_name', $indexName, $type)
         );
-       
-  
+
         $index->clearObjects()->wait();
-  
+    
         $paged = 1;
         $count = 0;
-  
+    
         do {
             $posts = new WP_Query([
                 'posts_per_page' => 100,
                 'paged' => $paged,
-                'post_type' => 'post',
+                'post_type' => $type,
                 'post_status' => 'publish',
-            ]);        
-  
+            ]);
+    
             if (!$posts->have_posts()) {
                 break;
             }
-  
+    
             $records = [];
-  
+    
             foreach ($posts->posts as $post) {
                 if (!empty($assoc_args['verbose'])) {
-                    WP_CLI::line('Serializing ['.$post->post_title.']');
+                    WP_CLI::line('Indexing ['.$post->post_title.']');
                 }
-                $record = (array) apply_filters('post_to_record', $post);
-  
-                if (!isset($record['objectID'])) {
+                $record = (array) apply_filters($type.'_to_record', $post);
+    
+                if (! isset($record['objectID'])) {
                     $record['objectID'] = implode('#', [$post->post_type, $post->ID]);
                 }
-  
+    
                 $records[] = $record;
                 $count++;
             }
-  
-            if (!empty($assoc_args['verbose'])) {
-                WP_CLI::line('Sending batch');
-            }
-  
+    
             $index->saveObjects($records);
-  
+    
             $paged++;
-  
+    
         } while (true);
-  
-        WP_CLI::success("$count posts indexed in Algolia");
-    }
-  }
+    
+        WP_CLI::success("$count $type entries indexed in Algolia");
+    }    
+}
   
 
 WP_CLI::add_command('algolia', 'Algolia_Command');
