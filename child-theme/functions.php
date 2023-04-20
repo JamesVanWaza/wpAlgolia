3c42f4a49c0f89d154b63931433af04a2d47bcef
JamesVanWaza <jgacuca@gmail.com>
2023-04-20 15:52:36 -0400

    Added the post_to_record function

--- child-theme/functions.php
+++ child-theme/functions.php
@@ -7,3 +7,24 @@ function algolia_post_index_name($defaultName) {
 }
 add_filter('algolia_index_name', 'algolia_post_index_name');
 
+function algolia_post_to_record(WP_Post $post) {
+    $tags = array_map(function (WP_Term $term) {
+        return $term->name;
+    }, wp_get_post_terms($post->ID, 'post_tag'));
+
+    return [
+        'objectID' => implode('#', [$post->post_type, $post->ID]),
+        'title' => $post->post_title,
+        'author' => [
+            'id' => $post->post_author,
+            'name' => get_user_by('ID', $post->post_author)->display_name,
+        ],
+        'excerpt' => $post->post_excerpt,
+        'content' => strip_tags($post->post_content),
+        'tags' => $tags,
+        'url' => get_post_permalink($post->ID),
+        'custom_field' => get_post_meta($post->id, 'custom_field_name'),
+    ];
+}
+add_filter('post_to_record', 'algolia_post_to_record');
+
