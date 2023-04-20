42da8b88f2fba6fed42f3f56bea5e7a12b733ee5
JamesVanWaza <jgacuca@gmail.com>
2023-04-20 15:51:01 -0400

    Created wp-cli and aci page

--- wp-content/plugins/algolia-custom-integration.php
+++ wp-content/plugins/algolia-custom-integration.php
@@ -9,4 +9,13 @@
  * @package         Algolia_Custom_Integration
  */
 
-// Your code starts here.
+// If you're using Composer, require the Composer autoload
+require_once __DIR__ . '/vendor/autoload.php';
+
+global $algolia;
+
+$algolia = \Algolia\AlgoliaSearch\SearchClient::create("InsertAPIForProject", "YourAdminApiKey");
+
+require_once __DIR__ . '/wp-cli.php';
+
+
